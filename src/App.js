f8756356f2822f86ae1e55b8063f75434ff2f06e
yanzhi@ty <tyktiania@126.com>
2020-12-31 17:59:32 +0800

    123

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import Xin from './assets/images/xiaoxin.jpg'
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Xin} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Test
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
