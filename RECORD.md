b7826c185eed9c41458a43c36e1d02cbb5b13d14
yanzhi <tyktianxia@gmail.com>
2021-03-07 23:05:15 +0800

    尝试react-router

---
+++ RECORD.md
@@ -0,0 +1,29 @@
+[react-router](https://reactrouter.com/web/guides/quick-start)  
+[react](https://react.docschina.org/docs/getting-started.html)  
+
++ react 17.x  
++ react-router-dom 5.x
+
+***
+## 需要学习的
++ 路由
++ 配置式路由* /分布式路由
++ react hooks
++ redux
++ ts jsx tsx
++ ...
+
+目标
+===
++ 用cra模拟antd pro 中后台管理系统
+    + 路由权鉴
+    + +antd
+    + 按需加载
+    + 首屏渲染
+    + mock数据
++ webpack
++ 用后台框架连接数据库开发
+
+
+yanzhi
+===
--- src/App.js
+++ src/App.js
@@ -2,22 +2,81 @@
 import Xin from './assets/images/xiaoxin.jpg'
 import { Button } from 'antd';
 import './assets/css/App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import AuthExample from './AuthExample'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={Xin} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Test
-        </a>
-        <Button type="primary">Button</Button>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={Xin} className="App-logo" alt="logo" />
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn Test
+    //     </a>
+    //     <Button type="primary">Button</Button>
+    //   </header>
+    // </div>
+    // <Router>
+    //   <div>
+    //     <ul>
+    //       <li>
+    //         <Link to="/">Home</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/about">About</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/dashboard">Dashboard</Link>
+    //       </li>
+    //     </ul>
+    //   </div>
+    //   <hr></hr>
+    //   <Switch>
+    //     <Route exact path="/">
+    //       <Home />
+    //     </Route>
+    //     <Route path="/about">
+    //       <About />
+    //     </Route>
+    //     <Route path="/dashboard">
+    //       <Dashboard />
+    //     </Route>
+    //   </Switch>
+    // </Router>
+    <AuthExample></AuthExample>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
     </div>
   );
 }
