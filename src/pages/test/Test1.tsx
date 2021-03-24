8cc3ce98fb5e871a7799285e8b45048a92a95425
yanzhi <tyktianxia@gmail.com>
2021-03-25 00:42:53 +0800

    testhtml

--- src/pages/test/Test1.tsx
+++ src/pages/test/Test1.tsx
@@ -12,29 +12,30 @@ import { incrementAction, reduceAction } from '../../reducers/calculate';
 import { connect } from 'react-redux';
 
 interface Props {
-    num: number,
-    increment: ()=>any,
-    decrement: ()=>any,
+  num: number,
+  increment: () => any,
+  decrement: () => any,
 }
 
 const mapStateToProps = (state: any) => {
-    return {
-      num: state.calculate.num
-    };
+  return {
+    num: state.calculate.num
   };
-  
-  const mapDispatchToProps = (dispatch: any) => ({
-    increment: () => dispatch(incrementAction),
-    decrement: () => dispatch(reduceAction)
-  });
+};
 
-@(connect( mapStateToProps, mapDispatchToProps, ) as any)
+const mapDispatchToProps = (dispatch: any) => ({
+  increment: () => dispatch(incrementAction),
+  decrement: () => dispatch(reduceAction)
+});
+
+@(connect(mapStateToProps, mapDispatchToProps,) as any)
 export default class Test1 extends Component<Props, any> {
-    render() {
-        return <div className='container'>
-            <p onClick={this.props.increment}>click to increment num</p>
-            <p onClick={this.props.decrement}>click to decrement num</p>
-            <p>{this.props.num}</p>
-        </div>
-    }
+  render() {
+    console.log(this)
+    return <div className='container'>
+      <p onClick={this.props.increment}>click to increment num</p>
+      <p onClick={this.props.decrement}>click to decrement num</p>
+      <p>{this.props.num}</p>
+    </div>
+  }
 }
